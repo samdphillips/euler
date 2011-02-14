071141f53fe8f401290ef6e960abdce8406a92fc
Sam Phillips <samdphillips@gmail.com>
2011-02-14 09:09:56 -0800

    Don't need racket/generator anymore

--- 0005.rkt
+++ 0005.rkt
@@ -8,8 +8,7 @@ What is the smallest positive number that is evenly divisible by all
 of the numbers from 1 to 20?
 |#
 
-(require racket/stream
-         racket/generator)
+(require racket/stream)
 
 (define (in-primes)
   (make-do-sequence
