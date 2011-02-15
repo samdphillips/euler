22fd30ae0fe57c0cc66bd4d91b3623e3c3a9358c
Sam Phillips <samdphillips@gmail.com>
2011-02-14 16:18:54 -0800

    more problem 4

--- 0004.rkt
+++ 0004.rkt
@@ -8,3 +8,20 @@ palindrome made from the product of two 2-digit numbers is
 Find the largest palindrome made from the product of two 
 3-digit numbers.
 |#
+
+(require racket/stream)
+
+(define (digits n)
+  (stream-map
+   (lambda (i) (- (sub1 (expt 10 n)) i))
+   (in-range (* 9 (expt 10 (sub1 n))))))
+
+(define (reverse-num n [a 0])
+  (if (< n 10)
+      (+ n (* a 10))
+      (let-values ([(q r) (quotient/remainder n 10)])
+        (reverse-num q (+ r (* a 10))))))
+
+
+
+     
