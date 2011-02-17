6cae9643869529ef983b0ecc9e935d93a8740c68
Sam Phillips <samdphillips@gmail.com>
2011-02-16 19:36:52 -0800

    This is much faster.

--- 0007.rkt
+++ 0007.rkt
@@ -7,27 +7,15 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and
 What is the 10001st prime number?
 |#
 
-(require racket/stream)
+(define (sieve p prime?)
+  (lambda (n)
+    (if (zero? (modulo n p)) #f (prime? n))))
 
-(define (in-primes)
-  (make-do-sequence
-   (lambda ()
-     (values 
-      (lambda (s)
-        (stream-first s))        
-      (lambda (s)
-        (let ([p (stream-first s)])
-          (stream-filter
-           (lambda (n)
-             (< 0 (modulo n p)))
-           (stream-rest s))))
-      (in-naturals 2)
-      (lambda a #t)
-      (lambda a #t)
-      (lambda a #t)))))
+(define (next-prime n prime?)
+  (or (prime? n) (next-prime (add1 n) prime?)))
 
-(define (solve n)
-  (stream-ref (in-primes)
-              (sub1 n)))
-
-(solve 10001)
+(define (solve [i 10001] [n 2] [prime? values])
+  (if (zero? i)
+      n
+      (let ([p (next-prime n prime?)])
+        (solve (sub1 i) p (sieve p prime?)))))
