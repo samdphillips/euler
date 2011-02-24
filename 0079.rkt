ecb5912e51e52cdeb3d5d822cbbfd272fe9532d6
Sam Phillips <samdphillips@gmail.com>
2011-02-23 16:16:14 -0800

    Much simpler solution to problem 79.

--- 0079.rkt
+++ 0079.rkt
@@ -14,11 +14,9 @@ analyse the file so as to determine the shortest possible
 secret passcode of unknown length.
 |#
 
-(require racket/list
+(require racket/match
+         racket/list
          racket/set)
-
-(struct leaf (rest) #:transparent)
-(struct node (value children) #:transparent)
 
 (define (digits n)
   (let-values ([(a r) (quotient/remainder n 100)])
@@ -26,52 +24,56 @@ secret passcode of unknown length.
       (list a b c))))
 
 (define init-state
-  (leaf
-   (let ([tries
-          (list 319 680 180 690 129 620 762 689 762 318 368 710 720
-                710 629 168 160 689 716 731 736 729 316 729 729 710
-                769 290 719 680 318 389 162 289 162 718 729 319 790
-                680 890 362 319 760 316 729 380 319 728 716)])
-     (for/list ([t (in-list tries)])
-       (digits t)))))
+  (list
+   (cons null         
+         (let ([tries
+                (list 319 680 180 690 129 620 762 689 762 318 
+                      368 710 720 710 629 168 160 689 716 731 
+                      736 729 316 729 729 710 769 290 719 680 
+                      318 389 162 289 162 718 729 319 790 680 
+                      890 362 319 760 316 729 380 319 728 716)])
+           (for/list ([t (in-list tries)])
+             (digits t))))))
 
-#|
+(define (sum x*) (apply + x*))
 
-(define (explore/step t)
-  (cond [(list? t) (map explore/step-node t)]
-        [(leaf? t) (explore/step-leaf t)]))
+(define (factorial n [a 1])
+  (if (zero? n) a (factorial (sub1 n) (* a n))))
 
-(define (explore/step-leaf lf)
-  (define r* (leaf-rest lf))
-  (define f* (for/set ([r (in-list r*)]) (first r)))
-  
-  (define (find-children f)
-    (leaf
-     (for/fold ([c* null]) ([r (in-list r*)])
-       (if (= f (first r))
-           (if (null? (cdr r))
-               c*
-               (cons (cdr r) c*))
-           (cons r c*)))))
-  
-  (for/list ([f (in-set f*)])
-    (node f (find-children f))))
+(define score
+  (match-lambda
+    [(cons _ x*)
+     (factorial (sum (map length x*)))]))
 
-(define (explore/step-node n)
-  (struct-copy node n
-    [children (explore/step (node-children n))]))
+(define (sort-score st*)  
+  (sort st* < #:key score #:cache-keys? #t))
 
-(define (fully-explored? t)
-  (cond [(null? t) #t]
-        [(list? t) (andmap fully-explored? t)]
-        [(leaf? t) #f]
-        [(node? t) (fully-explored? (node-children t))]
-        [else (error 'fully-explored "expected a tree got: ~a" t)]))
+(define expand
+  (match-lambda
+    [(cons s* r*)
+     
+     (define f* (for/set ([r (in-list r*)]) (first r)))
+     
+     (define (find-children f)
+       (for/fold ([c* null]) ([r (in-list r*)])
+         (if (= f (first r))
+             (if (null? (cdr r))
+                 c*
+                 (cons (cdr r) c*))
+             (cons r c*))))
+     
+     (for/list ([f (in-set f*)])
+       (cons (append s* (list f))
+             (find-children f)))]))
 
-(define (explore st)
-  (let loop ([st (explore/step st)])    
-    (let-values ([(c st) (partition fully-explored? st)])
-      (if (null? c)
-          (loop (explore/step st))
-          c))))
-|#
+
+(define (expand* st*)
+  (sort-score
+   (for/fold ([st* null]) ([st (in-list st*)])
+     (append st* (expand st)))))
+
+(define (solve [st* init-state])
+  (let ([st (car (expand* st*))])
+    (if (null? (cdr st))
+        (car st)
+        (solve (list st)))))
