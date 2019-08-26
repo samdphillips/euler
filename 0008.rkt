a947b4b7050ac36f89d6a24d3b052f2c38ae8c83
Sam Phillips <samdphillips@gmail.com>
2019-08-25 19:54:06 -0700

    Fixed and cleaned up 0008

--- 0008.rkt
+++ 0008.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 
 #|
-Find the greatest product of five consecutive digits in the 
+Find the greatest product of five consecutive digits in the
 1000-digit number.
 
 73167176531330624919225119674426574742355349194934
@@ -27,68 +27,83 @@ Find the greatest product of five consecutive digits in the
 
 |#
 
-(require (only-in racket/function const)
-         racket/stream)
+(module solution racket/base
+  (require racket/match
+           racket/stream
+           threading)
+  (provide (all-defined-out))
 
-(define the-number
-  (string-append 
-   "73167176531330624919225119674426574742355349194934"
-   "96983520312774506326239578318016984801869478851843"
-   "85861560789112949495459501737958331952853208805511"
-   "12540698747158523863050715693290963295227443043557"
-   "66896648950445244523161731856403098711121722383113"
-   "62229893423380308135336276614282806444486645238749"
-   "30358907296290491560440772390713810515859307960866"
-   "70172427121883998797908792274921901699720888093776"
-   "65727333001053367881220235421809751254540594752243"
-   "52584907711670556013604839586446706324415722155397"
-   "53697817977846174064955149290862569321978468622482"
-   "83972241375657056057490261407972968652414535100474"
-   "82166370484403199890008895243450658541227588666881"
-   "16427171479924442928230863465674813919123162824586"
-   "17866458359124566529476545682848912883142607690042"
-   "24219022671055626321111109370544217506941658960408"
-   "07198403850962455444362981230987879927244284909188"
-   "84580156166097919133875499200524063689912560717606"
-   "05886116467109405077541002256983155200055935729725"
-   "71636269561882670428252483600823257530420752963450"))
+  (define the-big-number
+    (string-append
+     "73167176531330624919225119674426574742355349194934"
+     "96983520312774506326239578318016984801869478851843"
+     "85861560789112949495459501737958331952853208805511"
+     "12540698747158523863050715693290963295227443043557"
+     "66896648950445244523161731856403098711121722383113"
+     "62229893423380308135336276614282806444486645238749"
+     "30358907296290491560440772390713810515859307960866"
+     "70172427121883998797908792274921901699720888093776"
+     "65727333001053367881220235421809751254540594752243"
+     "52584907711670556013604839586446706324415722155397"
+     "53697817977846174064955149290862569321978468622482"
+     "83972241375657056057490261407972968652414535100474"
+     "82166370484403199890008895243450658541227588666881"
+     "16427171479924442928230863465674813919123162824586"
+     "17866458359124566529476545682848912883142607690042"
+     "24219022671055626321111109370544217506941658960408"
+     "07198403850962455444362981230987879927244284909188"
+     "84580156166097919133875499200524063689912560717606"
+     "05886116467109405077541002256983155200055935729725"
+     "71636269561882670428252483600823257530420752963450"))
 
-(define (five-digits i)
-  (substring the-number i (+ i 5)))
+  (define (contains-zero? s)
+    (for/or ([c (in-string s)])
+      (char=? c #\0)))
 
-(define (find-zero s [i 4])
-  (define (check)
-    (and (char=? #\0 (string-ref s i)) i))
-  
-  (if (zero? i)
-      (check)
-      (or (check) 
-          (find-zero s (sub1 i)))))
+  (define (string->digits s)
+    (map (lambda (c)
+           (- (char->integer c) 48))
+         (string->list s)))
 
-(define (string->digits s)
-  (map (lambda (c) 
-         (- (char->integer c) 48))
-       (string->list s)))
+  (struct substring-stream
+    (string pos len)
+    #:methods gen:stream
+    [
+     (define (stream-empty? ss)
+       (match-define (substring-stream s pos len) ss)
+       (> (+ pos len) (string-length s)))
+     (define (stream-first ss)
+       (match-define (substring-stream s pos len) ss)
+       (substring s pos (+ pos len)))
+     (define (stream-rest ss)
+       (match-define (substring-stream s pos len) ss)
+       (substring-stream s (add1 pos) len))
+    ])
 
-(define the-numbers
-  (stream-map
-   (lambda (s)
-     (apply * (string->digits s)))
-   (stream-filter
-    (lambda (s)
-      (not (find-zero s)))
-    (make-do-sequence
-     (lambda ()
-       (values
-        (lambda (pos)
-          (five-digits pos))
-        add1
-        0
-        (lambda (pos)
-          (pos . < . 996))
-        (const #t)
-        (const #t)))))))  
+  (define (make-substring-stream s len)
+    (substring-stream s 0 len))
 
-(define (solve)
-  (for/fold ([i 0]) ([j the-numbers])
-    (max i j)))
+  (define (solve m)
+    (define the-numbers
+      (~>> (make-substring-stream the-big-number m)
+           (stream-filter (lambda~>> contains-zero? not))
+           (stream-map (lambda~>> string->digits (apply *)))))
+    (for/fold ([i 0]) ([j the-numbers])
+      (max i j)))
+)
+
+(module test "solutions.rkt"
+  0008
+  (require racket/stream
+           (submod ".." solution))
+
+  (let ([ss (make-substring-stream the-big-number 5)])
+    (check-equal? (stream-first ss) "73167")
+    (check-equal? (stream-first (stream-rest ss)) "31671"))
+
+  (check-false (contains-zero? "12345"))
+  (check-true  (contains-zero? "12340"))
+
+  (check-equal? (solve 4) 5832)
+  (check-equal? (solve 13) solution)
+)
