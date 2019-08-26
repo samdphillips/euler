c3c44befdf3d20b2e49d280b3cc9e1afff689191
Sam Phillips <samdphillips@gmail.com>
2019-08-25 17:25:10 -0700

    Alternative solution for #1

--- 0001.rkt
+++ 0001.rkt
@@ -8,14 +8,31 @@ is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 |#
 
-(define (mult-of? m n)
-  (zero? (modulo m n)))
+(module solution1 racket/base
+  (provide solve)
 
-(define (mult-of-3-or-5? n)
-  (or (mult-of? n 3)
-      (mult-of? n 5)))
+  (define (mult-of? m n)
+    (zero? (modulo m n)))
 
-(define (solve [m 1000])
-  (for/fold ([sum 0]) ([n (in-range m)]
-                       #:when (mult-of-3-or-5? n))
-    (+ sum n)))
+  (define (mult-of-3-or-5? n)
+    (or (mult-of? n 3)
+        (mult-of? n 5)))
+
+  (define (solve [m 1000])
+    (for/fold ([sum 0]) ([n (in-range m)]
+                         #:when (mult-of-3-or-5? n))
+      (+ sum n)))
+  )
+
+(module solution2 racket/base
+  (provide (all-defined-out))
+
+  (define (solve [m 1000])
+    (define (loop mult3 mult5 sum)
+      (cond
+        [(and (>= mult3 m) (>= mult5 m)) sum]
+        [(< mult3 mult5) (loop (+ 3 mult3) mult5 (+ mult3 sum))]
+        [(< mult5 mult3) (loop mult3 (+ 5 mult5) (+ mult5 sum))]
+        [else (loop (+ 3 mult3) (+ 5 mult5) (+ mult5 sum))]))
+    (loop 3 5 0))
+  )
