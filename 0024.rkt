ad0b8999d19b5fed34e0452bf446724f3e0411f7
Sam Phillips <samdphillips@gmail.com>
2024-12-09 17:38:58 -0800

    [0024] rewrite in a more modern style
    - rewrite in a more modern style
    - add some short explanatory comments
    - even though the lists are relatively short, removed some uses of `append`

--- 0024.rkt
+++ 0024.rkt
@@ -13,33 +13,42 @@ What is the millionth lexicographic permutation of the digits
 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 |#
 
-(require racket/function
-         racket/stream)
+(require racket/list)
 
-(define (integer->factoradic n d [m 2] [a '(0)])
-  (if (zero? n)
-      (if (< (length a) d)
-          (append (build-list (- d (length a)) (const 0))
-                  a)
-          a)
-      (let-values ([(q r) (quotient/remainder n m)])
-        (integer->factoradic q d (add1 m) (cons r a)))))
+(define (integer->factoradic n d)
+  (define (build-number n m a)
+    (cond
+      [(zero? n) a]
+      [else
+       (define-values (q r) (quotient/remainder n m))
+       (build-number q (add1 m) (cons r a))]))
+  (define (pad acc)
+    (define len (length acc))
+    (if (< len d)
+        (for/fold ([acc acc]) ([i (in-range len d)]) (cons 0 acc))
+        acc))
+  (pad (build-number n 2 '(0))))
 
-(define (pick n* i [head null])
-  (if (zero? i)
-      (values (car n*) 
-              (append (reverse head)
-                      (cdr n*)))
-      (pick (cdr n*) (sub1 i) (cons (car n*) head))))
+;; pick :: List.of(Any) * NonnegInt -> values(Any, List.of(Any)
+;; extracts the nth element of a List
+(define (pick n* i)
+  (cond
+    [(null? n*) (error 'pick "not enough values in list")]
+    [(zero? i) (values (car n*) (cdr n*))]
+    [else
+     (define-values (n tl) (pick (cdr n*) (sub1 i)))
+     (values n (cons (car n*) tl))]))
 
-(define (choose-digits i* n*)  
-  (if (null? i*)
-      null
-      (let-values ([(n n*) (pick n* (car i*))])
-        (cons n (choose-digits (cdr i*) n*)))))
+(define (choose-digits i* n*)
+  (cond
+    [(null? i*) null]
+    [else
+     (define-values (n n*^) (pick n* (car i*)))
+     (cons n (choose-digits (cdr i*) n*^))]))
 
+;; solve :: NonnegInt * NonnegInt -> List.of(Digit)
+;; Takes the permutation to generate and the number of digits to use
 (define (solve n m)
-  (let ([m* (stream->list (in-range 0 m))]
-        [f* (integer->factoradic (sub1 n) m)])
-    (choose-digits f* m*)))
-
+  (define m* (range 0 m))
+  (define f* (integer->factoradic (sub1 n) m))
+  (choose-digits f* m*))
